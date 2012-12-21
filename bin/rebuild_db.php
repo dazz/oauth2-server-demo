16721a2172e7d1726deaca0dce42de4e0e55c75e
dazz <annejulia@gmail.com>
2012-12-21 17:45:12 +0100

    added table oauth_users to rebuild_db.php script

--- bin/rebuild_db.php
+++ bin/rebuild_db.php
@@ -19,6 +19,8 @@ $db->exec('CREATE TABLE oauth_authorization_codes (authorization_code TEXT, clie
 $db->exec('CREATE TABLE oauth_refresh_tokens (refresh_token TEXT, client_id TEXT, user_id TEXT, expires TIMESTAMP, scope TEXT)');
 $db->exec('INSERT INTO oauth_clients (client_id, client_secret, redirect_uri) VALUES ("demoapp", "demopass", "http://localhost/demo/authorized")');
 
+$db->exec('CREATE TABLE oauth_users (username TEXT, password TEXT, first_name TEXT, last_name TEXT)');
+
 chmod($dir, 0777);
 // $db->exec('INSERT INTO oauth_access_tokens (access_token, client_id) VALUES ("testtoken", "Some Client")');
 // $db->exec('INSERT INTO oauth_authorization_codes (authorization_code, client_id) VALUES ("testcode", "Some Client")');
